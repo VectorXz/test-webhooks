67f50c385992fcd98a1639a213572ccdbdfa1a78
vectorxz <vectorx2263@hotmail.com>
2020-09-16 14:52:33 +0700

    update

--- github-hooks.php
+++ github-hooks.php
@@ -1,6 +1,9 @@
 <?php
 if(!isset($_POST["payload"])) {
     echo "works fine!";
+    $myfile = fopen("testfile.txt", "w");
+    fwrite($myfile, "Hello");
+    fclose($myfile);
 } else {
     $postBody = $_POST['payload'];
     $payload = json_decode($postBody);
