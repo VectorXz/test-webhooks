23c7632a0af2d9eb012e4220b9376b722b7e7368
vectorxz <vectorx2263@hotmail.com>
2020-09-16 14:47:33 +0700

    first commit

---
+++ github-hooks.php
@@ -0,0 +1,11 @@
+<?php
+if(!isset($_POST["payload"])) {
+    echo "works fine!";
+} else {
+    $postBody = $_POST['payload'];
+    $payload = json_decode($postBody);
+    $myfile = fopen("testfile.txt", "w");
+    fwrite($myfile, $payload);
+    fclose($myfile);
+}
+?>
